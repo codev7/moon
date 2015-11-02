6e9aa8624372d62ef1aac3ed0ebfecf9f6a2b9f7
huoy <ppdd@webrid.com>
2015-11-02 03:45:23 -0500

    add react-transform hmr+react-router

--- client/components/app.js
+++ client/components/app.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default
 class App extends React.Component {
 	render() {
 		return (
-			<div>Hello World!</div>
+			<div>
+				<Link to="/">Home</Link>
+				<Link to='/about'>About</Link>
+				<Link to='/contact'>Contact</Link>
+				{this.props.children}
+			</div>
 		)
 	}
 }
--- client/entry.js
+++ client/entry.js
@@ -1,12 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import App from './components/app.js'
+import About from './components/about.js'
+import Contact from './components/contact.js'
+import { Router, Route } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
 
 import './style.scss'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const routes = {
+  path: '/',
+  component: App,
+  childRoutes: [
+    { path: 'about', component: About },
+    { path: 'contact', component: Contact },
+  ]
+};
+
+ReactDOM.render(
+	(<Router
+		routes={routes}
+		history={createBrowserHistory()} />),
+		document.getElementById('app'))
 
 if(module.hot) {
-	module.hot.accept();
+  module.hot.accept();
 }
